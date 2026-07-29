321b057060d56e0741a467d7e085ba8d4529702d
nikkibarnwal <128201912+nikkibarnwal@users.noreply.github.com>
2026-07-29 12:26:48 +0530

    Refactor unique value extraction from array
    Refactor unique value extraction methods using filter, reduce, and Set.

--- java-script-topics/uniqueValueFromArr.js
+++ java-script-topics/uniqueValueFromArr.js
@@ -1,5 +1,18 @@
 const numbers = [1, 2, 2, 3, 4, 4, 5];
-const uniqueValue = numbers.filter((acc, value) => {
-  if (!acc.includes(value)) acc.push(value);
-}, []);
+const uniqueValue = numbers.filter((value,index,arr) => {
+  return arr.indexOf(value) === index
+});
 console.log(uniqueValue); // [1, 3, 5]
+
+
+
+const uniques= numbers.reduce((arr, item)=>{
+ if(!arr.includes(item)) arr.push(item);
+ return arr;
+},[])
+console.log(uniques);  // [1, 3, 5]
+
+
+
+const uniqueValue = [...new Set(numbers)];
+console.log(uniqueValue);
