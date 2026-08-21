6992554b7244a6aa61dd1d4a47669d6c3d450425
nikkibarnwal <128201912+nikkibarnwal@users.noreply.github.com>
2026-08-21 10:41:12 +0530

    Fix unique value extraction logic in uniqueValueFromArr.js
    Corrected the output of unique value calculations to include all unique numbers.

--- java-script-topics/uniqueValueFromArr.js
+++ java-script-topics/uniqueValueFromArr.js
@@ -2,7 +2,7 @@ const numbers = [1, 2, 2, 3, 4, 4, 5];
 const uniqueValue = numbers.filter((value,index,arr) => {
   return arr.indexOf(value) === index
 });
-console.log(uniqueValue); // [1, 3, 5]
+console.log(uniqueValue); // [1, 2, 3, 4, 5]
 
 
 
@@ -10,7 +10,7 @@ const uniques= numbers.reduce((arr, item)=>{
  if(!arr.includes(item)) arr.push(item);
  return arr;
 },[])
-console.log(uniques);  // [1, 3, 5]
+console.log(uniques);  // [1, 2, 3, 4, 5]
 
 
 
